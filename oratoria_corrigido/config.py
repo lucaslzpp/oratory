9f4d356adb06f946e83abc956ed2f13ed715700f
lucaslzpp <lucaslzpp@gmail.com>
2025-08-06 21:07:02 -0300

    config.py

--- oratoria_corrigido/config.py
+++ oratoria_corrigido/config.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 # Pega as configurações do secrets.toml
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+OPENAI_API_KEY = st.secrets["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"]
 GPT_MODEL = st.secrets.get("GPT_MODEL", "gpt-4")
